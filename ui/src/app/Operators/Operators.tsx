2c43c52a117ba9682e686c6e11e2771839b7559b
Graham Mainwaring <graham@mhn.org>
2021-11-21 00:39:25 -0500

    Prompt before delete

--- ui/src/app/Operators/Operators.tsx
+++ ui/src/app/Operators/Operators.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
-import { PageSection, Title, Button, Split, SplitItem, Modal, ModalVariant } from '@patternfly/react-core';
-import { TableComposable, Thead, Tr, Th, Td, Tbody } from '@patternfly/react-table';
+import {
+  Button,
+  ButtonVariant,
+  Dropdown,
+  DropdownItem,
+  KebabToggle,
+  Modal,
+  ModalVariant,
+  PageSection,
+  Split,
+  SplitItem,
+  Title
+} from '@patternfly/react-core';
+import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
 import { NamespaceSelector } from '@app/Namespaces/Namespaces';
-import { Dropdown, DropdownItem, KebabToggle } from '@patternfly/react-core';
+import { SimpleModal } from '@app/SimpleModal/SimpleModal';
 
 interface Operator {
   name: string
@@ -100,16 +112,20 @@ class OperatorActionsMenu extends React.Component<
   },
   {
     isOpen: boolean,
+    isDeleteModalOpen: boolean,
   }> {
   private readonly onToggle: (boolean) => void;
   private readonly onSelect: () => void;
   private readonly onFocus: () => void;
   private readonly onDeleteClick: () => void;
-  private readonly onChangeVersionClick: () => void;
+  private readonly onDeleteActionClick: () => void;
+  private readonly onDeleteModalClose: () => void;
+  private readonly onUpgradeClick: () => void;
   constructor(props) {
     super(props)
     this.state = {
-      isOpen: false
+      isOpen: false,
+      isDeleteModalOpen: false
     }
     this.onToggle = isOpen => {
       this.setState({
@@ -127,6 +143,11 @@ class OperatorActionsMenu extends React.Component<
       if (element) element.focus()
     }
     this.onDeleteClick = () => {
+      this.setState({
+        isDeleteModalOpen: true
+      })
+    }
+    this.onDeleteActionClick = () => {
       fetch(`/api/v1/operators/${this.props.namespace}`, {
         method: 'DELETE',
         headers: {
@@ -135,30 +156,48 @@ class OperatorActionsMenu extends React.Component<
         }
       })
     }
-    this.onChangeVersionClick = () => {
-      console.log("Change Version clicked")
+    this.onDeleteModalClose = () => {
+      this.setState({
+        isDeleteModalOpen: false
+      })
+    }
+    this.onUpgradeClick = () => {
+      console.log("Upgrade clicked")
     }
   }
 
   render() {
     const { isOpen } = this.state;
     const dropdownItems = [
-      <DropdownItem key="upgrade" component="button" onClick={this.onChangeVersionClick}>
-        Change Version
+      <DropdownItem key="upgrade" component="button" onClick={this.onUpgradeClick}>
+        Upgrade
       </DropdownItem>,
       <DropdownItem key="delete" component="button" onClick={this.onDeleteClick}>
         Delete
       </DropdownItem>
     ];
     return (
-      <Dropdown
-        onSelect={this.onSelect}
-        toggle={<KebabToggle onToggle={this.onToggle} id="toggle-id-6" />}
-        isOpen={isOpen}
-        isPlain
-        dropdownItems={dropdownItems}
-        position="right"
-      />
+      <React.Fragment>
+        <SimpleModal
+          title="Delete ClickHouse Operator?"
+          positionTop={true}
+          actionButtonText="Delete"
+          actionButtonVariant={ButtonVariant.danger}
+          isModalOpen={this.state.isDeleteModalOpen}
+          onActionClick={this.onDeleteActionClick}
+          onClose={this.onDeleteModalClose}
+        >
+        The operator will be removed from the <b>{this.props.namespace}</b> namespace.
+        </SimpleModal>
+        <Dropdown
+          onSelect={this.onSelect}
+          toggle={<KebabToggle onToggle={this.onToggle} id="toggle-id-6" />}
+          isOpen={isOpen}
+          isPlain
+          dropdownItems={dropdownItems}
+          position="right"
+        />
+      </React.Fragment>
     );
   }
 }
@@ -206,36 +245,34 @@ class OperatorTable extends React.Component<
     const columns = ['Name', 'Namespace', 'Status', 'Version']
     const column_fields = ['name', 'namespace', 'status', 'version']
     return (
-      <ul>
-        <TableComposable>
-          <Thead>
-            <Tr>
-              {
-                columns.map((column, columnIndex) => (
-                  <Th key={columnIndex}>{column}</Th>
-                ))
-              }
-              <Th key="menu"/>
-            </Tr>
-          </Thead>
-          <Tbody>
+      <TableComposable>
+        <Thead>
+          <Tr>
             {
-              this.state.operators.map((op, opindex) => (
-                <Tr key={opindex}>
-                  {
-                    columns.map((column, columnIndex) => (
-                      <Td key={columnIndex}>{op[column_fields[columnIndex]]}</Td>
-                    ))
-                  }
-                  <Td key="menu">
-                    <OperatorActionsMenu namespace={op.namespace} />
-                  </Td>
-                </Tr>
+              columns.map((column, columnIndex) => (
+                <Th key={columnIndex}>{column}</Th>
               ))
             }
-          </Tbody>
-        </TableComposable>
-      </ul>
+            <Th key="menu"/>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {
+            this.state.operators.map((op, opindex) => (
+              <Tr key={opindex}>
+                {
+                  columns.map((column, columnIndex) => (
+                    <Td key={columnIndex} dataLabel={column}>{op[column_fields[columnIndex]]}</Td>
+                  ))
+                }
+                <Td key="menu">
+                  <OperatorActionsMenu namespace={op.namespace} />
+                </Td>
+              </Tr>
+            ))
+          }
+        </Tbody>
+      </TableComposable>
     )
   }
 }
---
+++ ui/src/app/SimpleModal/SimpleModal.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Modal, Button, ButtonVariant, ModalVariant } from '@patternfly/react-core';
+
+class SimpleModal extends React.Component<
+  {
+    title: string
+    actionButtonText: string
+    actionButtonVariant?: ButtonVariant
+    cancelButtonText?: string
+    isModalOpen: boolean
+    onActionClick?: () => void
+    onClose: () => void
+    positionTop?: boolean
+  },
+  {
+    actionButtonVariant: ButtonVariant
+    cancelButtonText: string
+  }> {
+  private readonly closeModal: () => void;
+  private readonly actionClick: () => void;
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  constructor(props) {
+    super(props);
+    this.state = {
+      actionButtonVariant: this.props.actionButtonVariant ? this.props.actionButtonVariant : ButtonVariant.primary,
+      cancelButtonText: this.props.cancelButtonText ? this.props.cancelButtonText : "Cancel"
+    };
+    this.closeModal = () => {
+      this.props.onClose()
+    };
+    this.actionClick = () => {
+      this.closeModal()
+      if (this.props.onActionClick) {
+        this.props.onActionClick()
+      }
+    };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  render() {
+    return (
+      <React.Fragment>
+        <Modal
+          variant={ModalVariant.small}
+          position={this.props.positionTop ? "top" : undefined}
+          title={this.props.title}
+          isOpen={this.props.isModalOpen}
+          onClose={this.closeModal}
+          actions={[
+            <Button key="confirm" variant={this.state.actionButtonVariant} onClick={this.actionClick}>
+              {this.props.actionButtonText}
+            </Button>,
+            <Button key="cancel" variant="link" onClick={this.closeModal}>
+              {this.state.cancelButtonText}
+            </Button>
+          ]}
+        >
+          {this.props.children}
+        </Modal>
+      </React.Fragment>
+    );
+  }
+}
+
+export { SimpleModal }
