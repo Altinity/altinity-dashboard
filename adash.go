43cf440cb3f5984831e440c559b85b2646df5417
Graham Mainwaring <graham@mhn.org>
2021-12-14 14:04:58 -0500

    Move web server code out of main adash file

--- adash.go
+++ adash.go
@@ -2,30 +2,17 @@ package main
 
 import (
 	"context"
-	"crypto/rand"
 	"embed"
-	"encoding/base64"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/altinity/altinity-dashboard/internal/api"
 	_ "github.com/altinity/altinity-dashboard/internal/api"
-	"github.com/altinity/altinity-dashboard/internal/auth"
-	"github.com/altinity/altinity-dashboard/internal/certs"
+	"github.com/altinity/altinity-dashboard/internal/server"
 	"github.com/altinity/altinity-dashboard/internal/utils"
-	restfulspec "github.com/emicklei/go-restful-openapi/v2"
-	"github.com/emicklei/go-restful/v3"
-	"github.com/go-openapi/spec"
-	"io/fs"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
-	"regexp"
 	"runtime"
-	"strconv"
-	"time"
 )
 
 // App version info
@@ -97,213 +84,31 @@ func main() {
 		os.Exit(0)
 	}
 
-	// Check CLI flags for correctness
-	if (*tlsCert == "") != (*tlsKey == "") {
-		fmt.Printf("TLS cert and key must both be provided or neither")
-		os.Exit(1)
-	}
-	if (*selfSigned) && (*tlsCert != "") {
-		fmt.Printf("Cannot provide TLS certificate and also run self-signed")
-		os.Exit(1)
-	}
-
-	// Enable debug logging, if requested
-	if *debug {
-		api.ErrorsToConsole = true
-	}
-
-	// Connect to Kubernetes
-	err = utils.InitK8s(*kubeconfig)
-	if err != nil {
-		fmt.Printf("Could not connect to Kubernetes: %s\n", err)
-		os.Exit(1)
-	}
-
-	// If self-signed, generate the certificates
-	if *selfSigned {
-		cert, key, err := certs.GenerateSelfSignedCerts(true)
-		if err != nil {
-			fmt.Printf("Error generating self-signed certificate: %s\n", err)
-			os.Exit(1)
-		}
-		tlsCert = &cert
-		tlsKey = &key
-	}
-
-	// Determine default port, if one was not specified
-	if *bindPort == "" {
-		var port string
-		if *tlsCert != "" {
-			port = "8443"
-		} else {
-			port = "8080"
-		}
-		bindPort = &port
-	}
-
-	// Read the index.html from the bundled assets and update its devmode flag
-	var indexHTML []byte
-	indexHTML, err = uiFiles.ReadFile("ui/dist/index.html")
-	if err != nil {
-		panic(err)
-	}
-	for name, content := range map[string]string{
-		"devmode":      strconv.FormatBool(*devMode),
-		"version":      appVersion,
-		"chop-release": chopRelease,
-	} {
-		re := regexp.MustCompile(fmt.Sprintf(`meta name="%s" content="(\w*)"`, name))
-		indexHTML = re.ReplaceAll(indexHTML,
-			[]byte(fmt.Sprintf(`meta name="%s" content="%s"`, name, content)))
-	}
-
-	// Create HTTP router object
-	httpMux := http.NewServeMux()
-
-	// Create API handlers & docs
-	rc := restful.NewContainer()
-	rc.ServeMux = httpMux
-	wsi := api.WebServiceInfo{
-		Version:     appVersion,
+	// Start the server
+	c := server.Config{
+		TLSCert:     *tlsCert,
+		TLSKey:      *tlsKey,
+		SelfSigned:  *selfSigned,
+		Debug:       *debug,
+		Kubeconfig:  *kubeconfig,
+		BindHost:    *bindHost,
+		BindPort:    *bindPort,
+		DevMode:     *devMode,
+		NoToken:     *noToken,
+		AppVersion:  appVersion,
 		ChopRelease: chopRelease,
-		Embed:       &embedFiles,
+		UIFiles:     &uiFiles,
+		EmbedFiles:  &embedFiles,
 	}
-	for _, resource := range []api.WebService{
-		&api.DashboardResource{},
-		&api.NamespaceResource{},
-		&api.OperatorResource{},
-		&api.ChiResource{},
-	} {
-		var ws *restful.WebService
-		ws, err = resource.WebService(&wsi)
-		if err != nil {
-			fmt.Printf("Error initializing %s web service: %s\n", resource.Name(), err)
-			os.Exit(1)
-		}
-		rc.Add(ws)
-	}
-	config := restfulspec.Config{
-		WebServices:                   rc.RegisteredWebServices(), // you control what services are visible
-		APIPath:                       "/apidocs.json",
-		PostBuildSwaggerObjectHandler: enrichSwaggerObject}
-	rc.Add(restfulspec.NewOpenAPIService(config))
-
-	// Create handler for the CHI examples
-	examples, err := embedFiles.ReadDir("embed/chi-examples")
+	var ctx context.Context
+	ctx, err = c.RunServer()
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error: %s", err)
 	}
-	exampleStrings := make([]string, 0, len(examples))
-	for _, ex := range examples {
-		if ex.Type().IsRegular() {
-			exampleStrings = append(exampleStrings, ex.Name())
-		}
-	}
-	exampleIndex, err := json.Marshal(exampleStrings)
-	if err != nil {
-		panic(err)
-	}
-	subFilesChi, _ := fs.Sub(embedFiles, "embed/chi-examples")
-	subServerChi := http.StripPrefix("/chi-examples/", http.FileServer(http.FS(subFilesChi)))
-	httpMux.HandleFunc("/chi-examples/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/chi-examples/index.json" {
-			w.Header().Set("Content-Type", "application/json")
-			_, _ = w.Write(exampleIndex)
-		} else {
-			subServerChi.ServeHTTP(w, r)
-		}
-	})
-
-	// Create handler for the UI assets
-	subFiles, _ := fs.Sub(uiFiles, "ui/dist")
-	subServer := http.FileServer(http.FS(subFiles))
-	httpMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if (r.URL.Path == "/") ||
-			(r.URL.Path == "/index.html") ||
-			(r.URL.Path == "/operators") ||
-			(r.URL.Path == "/chis") ||
-			(r.URL.Path == "/devel") {
-			_, _ = w.Write(indexHTML)
-		} else {
-			subServer.ServeHTTP(w, r)
-		}
-	})
-
-	// Configure auth middleware
-	var httpHandler http.Handler
-	var authToken string
-	if *noToken {
-		httpHandler = httpMux
-	} else {
-		// Generate auth token
-		randBytes := make([]byte, 256/8)
-		_, err = rand.Read(randBytes)
-		if err != nil {
-			panic(err)
-		}
-		authToken = base64.RawURLEncoding.EncodeToString(randBytes)
-		httpHandler = auth.NewHandler(httpMux, authToken)
-	}
-
-	// Set up the server
-	bindStr := fmt.Sprintf("%s:%s", *bindHost, *bindPort)
-	var authStr string
-	if authToken != "" {
-		authStr = fmt.Sprintf("?token=%s", authToken)
-	}
-	var url string
-	var runServer func() error
-	if *tlsCert != "" {
-		url = fmt.Sprintf("https://%s%s", bindStr, authStr)
-		runServer = func() error {
-			return http.ListenAndServeTLS(bindStr, *tlsCert, *tlsKey, httpHandler)
-		}
-	} else {
-		url = fmt.Sprintf("http://%s%s", bindStr, authStr)
-		runServer = func() error {
-			return http.ListenAndServe(bindStr, httpHandler)
-		}
-	}
-
-	// Open the browser if requested and the server doesn't immediately fail
-	ctx, cancel := context.WithCancel(context.Background())
+	log.Printf("Server started.  Connect using: %s\n", c.URL)
 	if *openBrowser {
-		go func() {
-			select {
-			case <-ctx.Done():
-				return
-			case <-time.After(250 * time.Millisecond):
-				openWebBrowser(url)
-				return
-			}
-		}()
+		openWebBrowser(c.URL)
 	}
-
-	// Actually start the server
-	log.Printf("Server started.  Connect using: %s\n", url)
-	err = runServer()
-	cancel()
-	log.Fatal(err)
-}
-
-func enrichSwaggerObject(swo *spec.Swagger) {
-	swo.Info = &spec.Info{
-		InfoProps: spec.InfoProps{
-			Title: "Altinity Dashboard",
-			Contact: &spec.ContactInfo{
-				ContactInfoProps: spec.ContactInfoProps{
-					Name:  "Altinity",
-					Email: "info@altinity.com",
-					URL:   "https://altinity.com",
-				},
-			},
-			License: &spec.License{
-				LicenseProps: spec.LicenseProps{
-					Name: "Apache-2.0",
-					URL:  "https://www.apache.org/licenses/LICENSE-2.0",
-				},
-			},
-			Version: appVersion,
-		},
-	}
+	<-ctx.Done()
+	log.Fatalf("Error: %s", c.ServerError)
 }
---
+++ internal/server/server.go
@@ -0,0 +1,240 @@
+package server
+
+import (
+	"context"
+	"crypto/rand"
+	"embed"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/altinity/altinity-dashboard/internal/api"
+	"github.com/altinity/altinity-dashboard/internal/auth"
+	"github.com/altinity/altinity-dashboard/internal/certs"
+	"github.com/altinity/altinity-dashboard/internal/utils"
+	restfulspec "github.com/emicklei/go-restful-openapi/v2"
+	"github.com/emicklei/go-restful/v3"
+	"github.com/go-openapi/spec"
+	"io/fs"
+	"net/http"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+type Config struct {
+	TLSCert     string
+	TLSKey      string
+	SelfSigned  bool
+	Debug       bool
+	Kubeconfig  string
+	BindHost    string
+	BindPort    string
+	DevMode     bool
+	NoToken     bool
+	AppVersion  string
+	ChopRelease string
+	UIFiles     *embed.FS
+	EmbedFiles  *embed.FS
+	URL         string
+	ServerError error
+}
+
+var ErrTLSCertKeyBothOrNeither = errors.New("TLS cert and key must both be provided or neither")
+var ErrTLSOrSelfSigned = errors.New("cannot provide TLS certificate and also run self-signed")
+
+func (c *Config) RunServer() (context.Context, error) {
+	// Check CLI flags for correctness
+	if (c.TLSCert == "") != (c.TLSKey == "") {
+		return nil, ErrTLSCertKeyBothOrNeither
+	}
+	if (c.SelfSigned) && (c.TLSCert != "") {
+		return nil, ErrTLSOrSelfSigned
+	}
+
+	// Enable debug logging, if requested
+	if c.Debug {
+		api.ErrorsToConsole = true
+	}
+
+	// Connect to Kubernetes
+	err := utils.InitK8s(c.Kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to Kubernetes: %w", err)
+	}
+
+	// If self-signed, generate the certificates
+	if c.SelfSigned {
+		c.TLSCert, c.TLSKey, err = certs.GenerateSelfSignedCerts(true)
+		if err != nil {
+			return nil, fmt.Errorf("error generating self-signed certificate: %w", err)
+		}
+	}
+
+	// Determine default port, if one was not specified
+	if c.BindPort == "" {
+		if c.TLSCert != "" {
+			c.BindPort = "8443"
+		} else {
+			c.BindPort = "8080"
+		}
+	}
+
+	// Read the index.html from the bundled assets and update its devmode flag
+	var indexHTML []byte
+	indexHTML, err = c.UIFiles.ReadFile("ui/dist/index.html")
+	if err != nil {
+		return nil, fmt.Errorf("error reading embedded UI files: %w", err)
+	}
+	for name, content := range map[string]string{
+		"devmode":      strconv.FormatBool(c.DevMode),
+		"version":      c.AppVersion,
+		"chop-release": c.ChopRelease,
+	} {
+		re := regexp.MustCompile(fmt.Sprintf(`meta name="%s" content="(\w*)"`, name))
+		indexHTML = re.ReplaceAll(indexHTML,
+			[]byte(fmt.Sprintf(`meta name="%s" content="%s"`, name, content)))
+	}
+
+	// Create HTTP router object
+	httpMux := http.NewServeMux()
+
+	// Create API handlers & docs
+	rc := restful.NewContainer()
+	rc.ServeMux = httpMux
+	wsi := api.WebServiceInfo{
+		Version:     c.AppVersion,
+		ChopRelease: c.ChopRelease,
+		Embed:       c.EmbedFiles,
+	}
+	for _, resource := range []api.WebService{
+		&api.DashboardResource{},
+		&api.NamespaceResource{},
+		&api.OperatorResource{},
+		&api.ChiResource{},
+	} {
+		var ws *restful.WebService
+		ws, err = resource.WebService(&wsi)
+		if err != nil {
+			return nil, fmt.Errorf("error initializing %s web service: %w", resource.Name(), err)
+		}
+		rc.Add(ws)
+	}
+	config := restfulspec.Config{
+		WebServices:                   rc.RegisteredWebServices(), // you control what services are visible
+		APIPath:                       "/apidocs.json",
+		PostBuildSwaggerObjectHandler: c.enrichSwaggerObject}
+	rc.Add(restfulspec.NewOpenAPIService(config))
+
+	// Create handler for the CHI examples
+	examples, err := c.EmbedFiles.ReadDir("embed/chi-examples")
+	if err != nil {
+		return nil, fmt.Errorf("error reading embedded examples: %w", err)
+	}
+	exampleStrings := make([]string, 0, len(examples))
+	for _, ex := range examples {
+		if ex.Type().IsRegular() {
+			exampleStrings = append(exampleStrings, ex.Name())
+		}
+	}
+	exampleIndex, err := json.Marshal(exampleStrings)
+	if err != nil {
+		return nil, fmt.Errorf("error reading example index JSON: %w", err)
+	}
+	subFilesChi, _ := fs.Sub(c.EmbedFiles, "embed/chi-examples")
+	subServerChi := http.StripPrefix("/chi-examples/", http.FileServer(http.FS(subFilesChi)))
+	httpMux.HandleFunc("/chi-examples/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/chi-examples/index.json" {
+			w.Header().Set("Content-Type", "application/json")
+			_, _ = w.Write(exampleIndex)
+		} else {
+			subServerChi.ServeHTTP(w, r)
+		}
+	})
+
+	// Create handler for the UI assets
+	subFiles, _ := fs.Sub(c.UIFiles, "ui/dist")
+	subServer := http.FileServer(http.FS(subFiles))
+	httpMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if (r.URL.Path == "/") ||
+			(r.URL.Path == "/index.html") ||
+			(r.URL.Path == "/operators") ||
+			(r.URL.Path == "/chis") ||
+			(r.URL.Path == "/devel") {
+			_, _ = w.Write(indexHTML)
+		} else {
+			subServer.ServeHTTP(w, r)
+		}
+	})
+
+	// Configure auth middleware
+	var httpHandler http.Handler
+	var authToken string
+	if c.NoToken {
+		httpHandler = httpMux
+	} else {
+		// Generate auth token
+		randBytes := make([]byte, 256/8)
+		_, err = rand.Read(randBytes)
+		if err != nil {
+			return nil, fmt.Errorf("error generating random number: %w", err)
+		}
+		authToken = base64.RawURLEncoding.EncodeToString(randBytes)
+		httpHandler = auth.NewHandler(httpMux, authToken)
+	}
+
+	// Set up the server
+	bindStr := fmt.Sprintf("%s:%s", c.BindHost, c.BindPort)
+	var authStr string
+	if authToken != "" {
+		authStr = fmt.Sprintf("?token=%s", authToken)
+	}
+	var runServer func() error
+	if c.TLSCert != "" {
+		c.URL = fmt.Sprintf("https://%s%s", bindStr, authStr)
+		runServer = func() error {
+			return http.ListenAndServeTLS(bindStr, c.TLSCert, c.TLSKey, httpHandler)
+		}
+	} else {
+		c.URL = fmt.Sprintf("http://%s%s", bindStr, authStr)
+		runServer = func() error {
+			return http.ListenAndServe(bindStr, httpHandler)
+		}
+	}
+
+	// Start the server, but capture errors if it immediately fails to start
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		c.ServerError = runServer()
+		cancel()
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, c.ServerError
+	case <-time.After(250 * time.Millisecond):
+		return ctx, nil
+	}
+}
+
+func (c *Config) enrichSwaggerObject(swo *spec.Swagger) {
+	swo.Info = &spec.Info{
+		InfoProps: spec.InfoProps{
+			Title: "Altinity Dashboard",
+			Contact: &spec.ContactInfo{
+				ContactInfoProps: spec.ContactInfoProps{
+					Name:  "Altinity",
+					Email: "info@altinity.com",
+					URL:   "https://altinity.com",
+				},
+			},
+			License: &spec.License{
+				LicenseProps: spec.LicenseProps{
+					Name: "Apache-2.0",
+					URL:  "https://www.apache.org/licenses/LICENSE-2.0",
+				},
+			},
+			Version: c.AppVersion,
+		},
+	}
+}
